9183bb5ae031d030c3c512fad0069b45b7bf3daf
Nguyen Minh Anh <katnguyen500@gmail.com>
2022-09-05 01:21:22 +0700

    update demo 4
    update responsive,  js btn on going

---
+++ demo4/resources/main.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mysidenav").style.display = "block";
+    // document.getElementsByClassName("menu").style.backgroundColor = "rgba(0, 0, 0, 0.2) !important";
+}
+
+function closeNav() {
+    document.getElementById("mysidenav").style.display = "none";
+}
